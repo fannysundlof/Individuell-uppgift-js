31077be996deb252b2ee17dbbfcd02e0c1e925a5
Fanny sundlof <fanny.sundlof@medieinstitutet.se>
2019-12-16 12:28:51 +0100

    Klart

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 
 //Inkomst uträkning
-const incomebtn = document.querySelector(".input1__btn");
 
+// Väljer knappen som ska trigga inkomst-funktionen
+const incomebtn = document.querySelector(".input1__btn");
+//Funktionen som visar och räknar ut den totala inkomsten
 function Displayincome(e) {
+//Behövs för att sidan inte ska ladda om så fort funktionen körts
     e.preventDefault();
     const incomeNum = document.querySelector(".input1__value").value;
     const divInput = document.querySelector(".income__value");
-
+//Lägger den senaste inmatade inkomsten i en lista
     divInput.innerHTML ="<li>"+ incomeNum + "</li>";
 
-    const incometotal = document.querySelector(".income_tot");
-    
+//Räknar ut den totala inkomsten genom att addera alla värden som läggs in i income input
     incometotal.innerHTML = (Number(incometotal.innerHTML)) + (Number(incomeNum));
 
+//Räknar ut vinsten av totala inkomsten och utgifterna
+    const incometotal = document.querySelector(".income_tot");
+    const coststotal = document.querySelector(".costs_tot");
     const result = document.querySelector(".result_total");
     result.innerHTML = (Number(incometotal.innerHTML)) - (Number(coststotal.innerHTML));
 }
@@ -20,29 +25,29 @@ function Displayincome(e) {
     incomebtn.addEventListener("click", Displayincome);
 
 //Utgifter
+
+//Jag har gjort samma funktion för att räkna ut utgifter som inkomster. 
+
 const costsbtn = document.querySelector(".input2__btn");
     
     function Displaycosts(e) {
         e.preventDefault();
         const divInput = document.querySelector(".costs__value");
-        
         const costsNum = document.querySelector(".input2__value").value;
     
         divInput.innerHTML = "<li>"+ costsNum + "</li>"
-    
-        const coststotal = document.querySelector(".costs_tot");
         
         coststotal.innerHTML = (Number(coststotal.innerHTML)) + (Number(costsNum));
 
+        const coststotal = document.querySelector(".costs_tot");
+        const incometotal = document.querySelector(".income_tot");
         const result = document.querySelector(".result_total");
         result.innerHTML = (Number(incometotal.innerHTML)) - (Number(coststotal.innerHTML));
     }
+
         costsbtn.addEventListener("click", Displaycosts);
     
-    
-const result = document.querySelector(".result_total");
-const coststotal = document.querySelector(".costs_tot");
-const incometotal = document.querySelector(".income_tot");
 
-result.innerHTML = (Number(incometotal.innerHTML)) - (Number(coststotal.innerHTML));
+
+
 
